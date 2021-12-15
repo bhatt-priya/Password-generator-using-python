d61c424bca3710dc280a5fe249dc8818ac015af7
PRIYA BHATT <41810819+bhatt-priya@users.noreply.github.com>
2021-12-15 13:16:31 +0530

    Update PASSWORD GENERATOR.py

--- PASSWORD GENERATOR.py
+++ PASSWORD GENERATOR.py
@@ -1,21 +1,21 @@
 
-import casesensitive
-import upper_lower_symbol_number
-print("Enter Choice=1 for casesensetive password that is without capital letters")
-print("Enter Choice=2 for upper case,lower case,symbol,number included password")
-while True:
-    print("")
-    c=int(input("Enter your choice:"))
-    if(c==1):
-        casesensitive.casesensitive()
-        break
-    elif(c==2):
-        upper_lower_symbol_number.upper_lower_symbol_number()
-        break
-    else:
-        print("Invalid choice")
-ans=input("Do you want to run again?<y/n>:")
-if ans=="n":
-    print("")
-    sys.exit()
+import random
+
+char1="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#!$%"
+while 1:
+    print("PLEASE ENTER THE DATA BELOW AS NUMBERS")
+    password_len=int(input("Length of password?"))
+    password_count=int(input("How many password you would like to generate"))
+    for x in range(0,password_count):
+        password=""
+        for x in range(0,password_len):
+            password_char=random.choice(char1)
+                
+
+            password=password+password_char
+            print("Here it is:",password)
+            
+
+    
+        
 
